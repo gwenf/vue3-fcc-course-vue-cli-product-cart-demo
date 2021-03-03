1d3c300478bebeb6d11aa1e196a680365713ba2a
gwenf <gwenfaraday@pm.me>
2021-03-03 10:59:06 -0500

    added css and sass to application

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// import './assets/styles/main.css'
+import './assets/styles/style.scss'
+
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 import Products from '../views/Products.vue'
+import PastOrders from '../views/PastOrders.vue'
 
 const routes = [
   {
@@ -12,6 +13,11 @@ const routes = [
     path: '/products',
     name: 'Products',
     component: Products
+  },
+  {
+    path: '/past-orders',
+    name: 'PastOrders',
+    component: PastOrders
   }
 ]
 
